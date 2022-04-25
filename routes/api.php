25caa553e997af923ec3b7d05fee8076e36683f0
Oleh Popkov <olehpopkov@MacBook-Pro-Oleh.local>
2022-04-25 12:24:37 +0300

    stage 1 (registration form on VueJS)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Fruit\IndexController;
+use App\Http\Controllers\User\StoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'User', 'prefix' => 'users'], function (){
+    Route::post('/', [StoreController::class, '__invoke']);
+});
+
+Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function (){
+    Route::get('/', [IndexController::class, '__invoke']);
 });
